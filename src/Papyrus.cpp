f328566cea4d0656666d5ea65f59b65a4d726733
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-08-23 21:37:56 -0700

    Update Papyrus.cpp

--- src/Papyrus.cpp
+++ src/Papyrus.cpp
@@ -2,9 +2,6 @@
 
 #include "skse64/GameReferences.h"
 
-#include <cstdlib>  // size_t
-#include <string>  // string
-
 #include "RE/Skyrim.h"
 #include "REL/Relocation.h"
 
@@ -19,10 +16,6 @@ namespace ConsoleUtil
 
 	void ExecuteCommand(RE::StaticFunctionTag*, RE::BSFixedString a_command)
 	{
-		// 41 54 41 56 41 57 48 83 EC 40 48 C7 44 24 38 FE FF FF FF 48 89 5C 24 60 48 89 6C 24 68 48 89 74 24 70 48 89 7C 24 78 4D 8B F1
-		using func_t = void(RE::Script*, RE::ConsoleManager*, std::size_t, RE::TESObjectREFR*);
-		REL::Offset<func_t*> func(0x002E75F0);	// 1_5_80
-
 		auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>();
 		if (!scriptFactory) {
 			return;
@@ -37,10 +30,8 @@ namespace ConsoleUtil
 		RE::TESObjectREFRPtr selectedRef;
 		RE::TESObjectREFR::LookupByHandle(handle, selectedRef);
 
-		std::string cmd = a_command.c_str();
-		script->unk38 = cmd.data();
-		func(script, RE::ConsoleManager::GetSingleton(), 1, selectedRef.get());
-		script->unk38 = 0;
+		script->SetCommand(a_command);
+		script->Invoke(selectedRef.get());
 		delete script;
 	}
 
